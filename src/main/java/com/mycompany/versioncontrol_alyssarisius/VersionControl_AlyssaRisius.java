584a4fb392d4a2189511c1a0bbada0d71f880ec8
Alyssa Risius <alyssa.risius@gmail.com>
2024-09-25 18:53:46 -0500

    Initial Commit

---
+++ src/main/java/com/mycompany/versioncontrol_alyssarisius/VersionControl_AlyssaRisius.java
@@ -0,0 +1,12 @@
+package com.mycompany.versioncontrol_alyssarisius;
+
+/**
+ *
+ * @author Alyssa
+ */
+public class VersionControl_AlyssaRisius {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
